afc7f03468798b14c777202cd7a361dbf73781e6
seongolee <seongo0521@gmail.com>
2022-05-10 08:18:03 +0900

    feat: React 연습 Create 기능 추가
    Create 컴포넌트를 만들고, 로직까지 추가.

---
+++ src/practice/Create.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Create = (props) => {
+    return (
+      <article>
+          <h2>Create</h2>
+          <form onSubmit={(e) => {
+              e.preventDefault();
+              const title = e.target.title.value;
+              const body = e.target.body.value;
+              props.onCreate(title, body);
+
+          }}>
+              <p><input type="text" name="title" placeholder="title"/></p>
+              <p><textarea name="body" placeholder="body"></textarea></p>
+              <p><input type="submit" value="Create" /></p>
+          </form>
+      </article>
+    );
+}
+
+export default Create;
--- src/practice/PracticeApp.js
+++ src/practice/PracticeApp.js
@@ -2,31 +2,59 @@ import React, {useState} from "react";
 import Header from "./Header";
 import Nav from "./Nav";
 import Article from "./Article";
+import Create from './Create';
 
 const PracticeApp = () => {
-    const mode = 'WELCOME';
-    const topics = [
+    const [mode, setMode] = useState('WELCOME');
+    const [id, setId] = useState(null);
+    const [nextId, setNextId] = useState(4);
+
+    const [topics, setTopics] = useState([
         {id:1, title:'html', body:'html is ....'},
         {id:2, title:'css', body:'css is ....'},
         {id:3, title:'javascript', body:'javascript is ....'},
-    ]
+    ]);
 
     let content = null;
     if(mode === 'WELCOME'){
         content = <Article title="Welcome" body="Hello, WEB"></Article>
-    } else if (mode === 'READ'){
-        content = <Article title="Read" body="Hello, Read"></Article>
+    } else if(mode === 'READ'){
+        let title, body = null;
+        for(let i=0; i<topics.length; i++){
+            if(topics[i].id === id){
+                title = topics[i].title;
+                body = topics[i].body;
+                break;
+            }
+        }
+        content = <Article title={title} body={body}></Article>
+    } else if(mode === 'CREATE'){
+        content = <Create onCreate={(_title, _body) => {
+            const newTopic = {id: nextId, title:_title, body:_body};
+            const newTopics = [...topics];
+            newTopics.push(newTopic);
+            setTopics(newTopics);
+            setId(nextId);
+            setNextId(nextId + 1);
+            setMode('READ')
+
+        }}/>
     }
 
     return (
         <div>
             <Header title="WEB" onChangeMode={() => {
-                alert('Header');
+                setMode('WELCOME');
             }}></Header>
             <Nav topics={topics} onChangeMode={(id) => {
-                alert(id);
+                setMode('READ');
+                setId(id);
             }}></Nav>
             {content}
+            <a href="/create" onClick={(e) => {
+                e.preventDefault();
+                setMode('CREATE');
+            }}>Create</a>
         </div>
     );
 }
